9be03c3617acd6ee40c4bf556bd1739e6db828d2
Søren Ditlefsen <soeren_ditlefsen@hotmail.com>
2020-08-11 09:04:47 +0200

    Test that die always rolls between one and six

--- src/test/TestDie.java
+++ src/test/TestDie.java
@@ -18,17 +18,31 @@ public class TestDie {
     @Test
     public void shouldReturnDieNumber() {
         d.setNumber(4);
-        assertThat(d.getNumber(),is(4));
+        assertThat(d.getNumber(), is(4));
     }
 
     @Test
     public void shouldRoll3To4OnAverage() {
         double sum = 0;
-        for(int i = 1; i <= 100; i++) {
+        for (int i = 1; i <= 100; i++) {
             d.roll();
             sum += d.getNumber();
         }
-        assertThat(sum,closeTo(350,100));
+        assertThat(sum, closeTo(350, 100));
     }
-    
+
+    @Test
+    public void shouldRollBetweenOneAndSix() {
+        int min = 10;
+        int max = 0;
+        for (int i = 1; i <= 100; i++) {
+            d.roll();
+            int n = d.getNumber();
+            if (n < min) min = n;
+            if (n > max) max = n;
+        }
+        assertThat(min, is(1));
+        assertThat(max, is(6));
+    }
+
 }
