93df36941b3aae6c149a783d695abcbf5e65021a
Søren Ditlefsen <soeren_ditlefsen@hotmail.com>
2020-11-05 22:42:19 +0100

    I deleted some lines in a test class

--- test/skejbydice/standard/TestGame.java
+++ test/skejbydice/standard/TestGame.java
@@ -12,29 +12,22 @@ public class TestGame {
     private Game game;
     private Player anders;
     private Player anna;
-    private Player bjarne;
-
-
     private RegularDie attackingDie1;
     private RegularDie attackingDie2;
     private RegularDie defendingDie;
-    private Player britta;
 
     @Before
     public void setUp() throws InvalidDieNumberException {
-
         anders = new Player("Anders");
         anna = new Player("Anna");
-
-        bjarne = new Player("Bjarne");
-        britta = new Player("Britta");
 
         attackingDie1 = new RegularDie(new FixedNumberRollStrategy(3)); //Always rolls 3
         attackingDie2 = new RegularDie(new FixedNumberRollStrategy(6)); //Always rolls 6
         defendingDie = new RegularDie(new FixedNumberRollStrategy(3));
 
         game = new Game(2, attackingDie1, attackingDie2, defendingDie);
-
+        game.addPlayer(anders);
+        game.addPlayer(anna);
     }
 
     @Test
@@ -44,15 +37,11 @@ public class TestGame {
 
     @Test
     public void firstPlayerAddedShouldStartTheGame() {
-        game.addPlayer(anders);
-        game.addPlayer(anna);
         assertThat(game.getPlayerInTurn(),is(anders));
     }
 
     @Test
     public void rollingTwiceShouldRoundUpSips() {
-        game.addPlayer(anders);
-        game.addPlayer(anna);
         game.startGame();
         game.rollAttackingDice();
         assertThat(game.getAttackingValue(),is(4)); //First time rolling 3+6 gives 4 sips away
@@ -62,8 +51,6 @@ public class TestGame {
 
     @Test
     public void rollingTwiceShouldCauseAttackingPlayerToDrinkSips() {
-        game.addPlayer(anders);
-        game.addPlayer(anna);
         game.startGame();
         game.rollAttackingDice();
         assertThat(game.getSipsFromActivePlayer(),is(0));
@@ -73,12 +60,10 @@ public class TestGame {
 
     @Test
     public void ifDefendingPlayerChoosesToDrinkFrom6And3AttackHeShouldDrink4Sips () {
-        game.addPlayer(bjarne);
-        game.addPlayer(britta);
         game.startGame();
         game.rollAttackingDice();
-        game.attackPlayer(britta);
-        assertThat(game.getPlayerUnderAttack(),is(britta));
+        game.attackPlayer(anna);
+        assertThat(game.getPlayerUnderAttack(),is(anna));
         game.punishLosingPlayer();
         assertThat(game.getSipsFromPlayerUnderAttack(),is(4));
     }
